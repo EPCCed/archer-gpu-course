92a66d1e6b593cf765ae730d59287ebcff18ee0e
Kevin Stratford <kevin@epcc.ed.ac.uk>
2020-11-22 14:09:10 +0000

    Update to SLURM

--- exercises/cuda-optimise/src_c/submit.sh
+++ exercises/cuda-optimise/src_c/submit.sh
@@ -1,27 +1,18 @@
 #!/bin/bash
 #
+# opencl-intro
+#
 # Join stdout and sterr in submit.o{job_id}
 # Set the queue and the resources
 #
-#PBS -N submit
-#PBS -j oe
-#PBS -q gpu-teach
-#PBS -l select=1:ncpus=10:ngpus=1
-#PBS -l walltime=0:01:00
 
-# Budget: use either your default or the reservation
-#PBS -A y15
+#SBATCH --job-name=submit
+#SBATCH --gres=gpu:1
+#SBATCH --time=00:01:00
+#SBATCH --partition=gpu-skylake
+#SBATCH --qos=gpu
 
-# Load the required modules
-module load gcc
-module load cuda
 
-# Pick a random device as PBS on Cirrus not yet configured to control
-# GPU visibility
-r=$RANDOM; let "r %= 4";
-export CUDA_VISIBLE_DEVICES=$r
 echo "CUDA_VISIBLE_DEVICES set to ${CUDA_VISIBLE_DEVICES}"
-
-cd $PBS_O_WORKDIR
 
 ./reconstruct
