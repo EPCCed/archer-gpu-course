91dcfac32c5df77dbc96986bf82546900106c10e
Kevin Stratford <kevin@epcc.ed.ac.uk>
2020-11-22 16:32:02 +0000

    Remove compiler warnings

--- exercises/opencl-optimise/reverse.c
+++ exercises/opencl-optimise/reverse.c
@@ -11,6 +11,10 @@ double get_current_time();
 void datread(char*, void*, int, int);
 void pgmwrite(char*, void*, int, int);
 void checkOpenCLError(cl_int, char *);
+cl_int initCLDevice(cl_device_type devtype, cl_context *ctxt,
+                    cl_command_queue *queue);
+cl_int getCLKernel(cl_context ctxt, char *filename, char *funcname,
+                   cl_kernel *kernel);
 
 /* Dimensions of image */
 #define WIDTH 2048
